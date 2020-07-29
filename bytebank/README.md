2d21fa0f07ad6eeae8c0fb091e3d0270a0507dd2
daviguelfi <daviguelfi@gmail.com>
2020-07-29 15:08:33 -0300

    create a form

--- bytebank/README.md
+++ bytebank/README.md
@@ -1,14 +1,21 @@
 # Fundamentos de Flutter: Crie o seu primeiro app
 
-## O que aprendi fazendo o _módulo 1 - Primeiros passas com o Flutter_ ?
+## O que aprendi fazendo o _módulo 1 - Primeiros passas com o Flutter_?
 
 * Primeiro contato com *dart* e o *flutter*
 * Como funciona os widgets
 * Como adicionar mais de um widget na tela
 
 
-## O que aprendi fazendo o _módulo 2 - Implementando layout com Widgets_ ?
+## O que aprendi fazendo o _módulo 2 - Implementando layout com Widgets_?
 
 * A refatorar o código do _Flutter_
 * Boas praticas de programação em _Flutter_
 * E como extrair os _widgets_, deixando eles como "components"
+
+
+## O que aprendi fazendo o _módulo 3 - Criando formulário e refatorando o código_?
+
+* Criar um formulário utilizando o _widget *TextField*_
+* Aplicar margem nos _widgets_ 
+* E como recuperar o valor dos _inputs_
--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,17 +1,85 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Transferências'),
+  runApp(BytebankApp());
+}
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
       ),
-      body: ListaTransferencias(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
-    ),
-  ));
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Criando Transferência"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _controladorCampoNumeroConta,
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Número da conta',
+                  hintText: '0000',
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _controladorCampoValor,
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+                decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: 'Valor da transferência',
+                  hintText: '0.00',
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                debugPrint('clicou no confirmar');
+                final int numeroConta =
+                    int.tryParse(_controladorCampoNumeroConta.text);
+                final double valor =
+                    double.tryParse(_controladorCampoValor.text);
+
+                if (numeroConta != null && valor != null) {
+                  final transferenciaCriada = Transferencia(valor, numeroConta);
+                  debugPrint('$transferenciaCriada');
+                  Scaffold.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('$transferenciaCriada'),
+                    ),
+                  );
+                }
+              },
+              child: Text('Confirmar'),
+            ),
+          ],
+        ));
+  }
 }
 
 class ListaTransferencias extends StatelessWidget {
@@ -20,18 +88,21 @@ class ListaTransferencias extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ItemTransferencia(Transferencia(100, 1000)),
-        ItemTransferencia(Transferencia(200, 2000)),
-        ItemTransferencia(Transferencia(300, 3000)),
-      ],
-    );
+    return Scaffold(
+        body: Column(
+          children: <Widget>[
+            ItemTransferencia(Transferencia(100, 1000)),
+            ItemTransferencia(Transferencia(200, 2000)),
+            ItemTransferencia(Transferencia(300, 3000)),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+        ));
   }
 }
 
 class ItemTransferencia extends StatelessWidget {
-
   final Transferencia _transferencia; // _ é o metodo privado
 
   ItemTransferencia(this._transferencia);
@@ -52,4 +123,9 @@ class Transferencia {
   final int numerConta;
 
   Transferencia(this.valor, this.numerConta);
+
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, numerConta: $numerConta}';
+  }
 }
